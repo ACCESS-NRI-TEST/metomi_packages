ce04a8cecd9c94d5f97e666687635a26d759d514
Davide Marchegiani <davide.marchegiani@anu.edu.au>
2025-07-22 23:39:13 +1000

    Updated mule recipe

--- packages/mule/install-mule.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-pushd ${SRC_DIR}/mule
-${PYTHON} -m pip install . -vv --no-deps --ignore-installed
-popd
--- packages/mule/install-um_packing.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-pushd ${SRC_DIR}/um_packing
-${PYTHON} -m pip install . -vv --no-deps --ignore-installed
-popd
