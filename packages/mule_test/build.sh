bd2b45fcac40cbdaa05e72fb4d496c967cb3ac80
Davide Marchegiani <davide.marchegiani@anu.edu.au>
2025-07-24 08:18:16 +1000

    Testing mule recipe

--- packages/mule_test/build.sh
+++ packages/mule_test/build.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 set -ex
 
-${PYTHON} -m pip install -vv --no-deps --no-build-isolation ./um_spiral_search
-${PYTHON} -m pip install -vv --no-deps --no-build-isolation ./um_packing
-${PYTHON} -m pip install -vv --no-deps --no-build-isolation ./mule
-${PYTHON} -m pip install -vv --no-deps --no-build-isolation ./um_ppibm
-${PYTHON} -m pip install -vv --no-deps --no-build-isolation ./um_utils
+if [[ "${build_platform}" == "linux-64" ]]; then
+    $PYTHON -m pip install -vv --no-deps --no-build-isolation ./um_spiral_search
+fi
