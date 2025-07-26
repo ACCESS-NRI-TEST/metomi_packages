76756636d451b2b8fadd3a2ca9e7865a2554c417
Davide Marchegiani <davide.marchegiani@anu.edu.au>
2025-07-26 13:44:16 +1000

    Deleted obsolete folders and renamed mule_test to mule

--- packages/mule_ngmo/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -ex
-
-${PYTHON} -m pip install -vv --no-deps --no-build-isolation ./um_spiral_search
-${PYTHON} -m pip install -vv --no-deps --no-build-isolation ./um_packing
-${PYTHON} -m pip install -vv --no-deps --no-build-isolation ./mule
-${PYTHON} -m pip install -vv --no-deps --no-build-isolation ./um_ppibm
-${PYTHON} -m pip install -vv --no-deps --no-build-isolation ./um_utils
--- packages/um_spiral_search/build.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-pushd ${SRC_DIR}/um_spiral_search
-${PYTHON} -m pip install . -vv --no-deps --ignore-installed
-popd
