b3a92d28cacfe53e4ffe4044a746d491324d5dfe
Davide Marchegiani <davide.marchegiani@anu.edu.au>
2025-08-07 12:38:21 +1000

    Fixed typo in workflow

--- packages/mule/get-version.sh
+++ packages/mule/get-version.sh
@@ -8,7 +8,7 @@ regex="^([^=\s]*)\s*=\s*${revision}$"
 # Get version from the SVN property fcm:revision
 version=$(svn propget fcm:revision $url | awk 'match($0, /'"$regex"'/, m) {print m[1]}')
 if [ -z "$version" ]; then
-  echo "mule version could not be retrieved from revision '$revision'."
+  echo "::error::mule version could not be retrieved from revision '$revision'." >&2
   exit 1
 fi
 # Remove any leading 'v' or 'vn'
