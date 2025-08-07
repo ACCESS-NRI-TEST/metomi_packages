311e4a3da0b4944f9c3ef348a50700a1a6541cae
Davide Marchegiani <davide.marchegiani@anu.edu.au>
2025-08-07 12:25:55 +1000

    Updated release-package workflow

---
+++ packages/shumlib/get-version.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -xe
+
+revision=$1
+url=https://code.metoffice.gov.uk/svn/utils/shumlib/
+regex="^([^=\s]*)\s*=\s*${revision}$"
+# Get version from the SVN property fcm:revision
+version=$(svn propget fcm:revision $url | awk 'match($0, /'"$regex"'/, m) {print m[1]}')
+if [ -z "$version" ]; then
+  echo "shumlib version could not be retrieved from revision '$revision'."
+  exit 1
+fi
+# Remove any leading 'v' or 'vn'
+version=${version#v}
+version=${version#n}
+echo "${version}"
